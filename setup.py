e47046746ec66079494430f3ff6b979e746e5373
Bogdan Tatarov <bogdan@tatarov.me>
2023-05-22 14:07:53 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,31 @@
+import codecs
+import os.path
 from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+# get __version__ from a file
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError('Unable to find version string.')
 
 setup(
     name='senpai-cli',
-    version='0.70a',
+    version=get_version('src/senpai/__init__.py'),
     description='BashSenpai command line interface',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
     url='https://BashSenpai.com/',
     author='Bogdan Tatarov',
     author_email='bogdan@tatarov.me',
@@ -12,6 +34,6 @@ setup(
     packages=find_packages(where='src'),
     package_dir = {'': 'src'},
     entry_points=dict(
-        console_scripts=['senpai=senpai:main']
+        console_scripts=['senpai=main:main']
     )
 )

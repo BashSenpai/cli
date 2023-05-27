213cbfe4f7a3ba762a59191fcef6a901f7216f4f
Bogdan Tatarov <bogdan.tatarov@gmail.com>
2023-05-27 19:05:51 +0300

    update metadata (brew)

--- brew/senpai-cli.rb
+++ brew/senpai-cli.rb
@@ -3,8 +3,8 @@ class SenpaiCli < Formula
 
   desc "BashSenpai command-line interface"
   homepage "https://bashsenpai.com/"
-  url "https://github.com/BashSenpai/cli/archive/refs/tags/v0.77b.tar.gz"
-  sha256 "73f8f8c6f79de420c58f441051f6825b2103b5f8ab37b5cef2722b778cce9f68"
+  url "https://github.com/BashSenpai/cli/archive/refs/tags/v0.78b.tar.gz"
+  sha256 "f9dbc80578c6c9b5e58b7139f709b8f2cc6f1c8eaf01b3f98a5a82a146bea9e4"
   license "Apache-2.0"
 
   depends_on "python@3.11"
@@ -17,6 +17,16 @@ class SenpaiCli < Formula
   resource "charset-nomalizer" do
     url "https://files.pythonhosted.org/packages/ff/d7/8d757f8bd45be079d76309248845a04f09619a7b17d6dfc8c9ff6433cac2/charset-normalizer-3.1.0.tar.gz"
     sha256 "34e0a2f9c370eb95597aae63bf85eb5e96826d81e3dcf88b8886012906f509b5"
+  end
+
+  resource "gnureadline" do
+    if Hardware::CPU.arm?
+      url "https://files.pythonhosted.org/packages/83/03/65d82e9290ae8a2a3b2285dc8aebd304437a6ba7ad03823438730525ab45/gnureadline-8.1.2-cp311-cp311-macosx_11_0_arm64.whl", :using => :nounzip
+      sha256 "74f2538ac15ff4ef9534823abdef077bb34c7dd343e204a36d978f09e168462f"
+    else
+      url "https://files.pythonhosted.org/packages/a7/f2/77195ef94f56b61ad881685e3a87cc39a9972e01ccacd555acaa001a92a0/gnureadline-8.1.2-cp311-cp311-macosx_10_9_x86_64.whl", :using => :nounzip
+      sha256 "c1bcb32e3b63442570d6425055aa6d5c3b6e8b09b9c7d1f8333e70203166a5a3"
+    end
   end
 
   resource "idna" do
@@ -43,14 +53,19 @@ class SenpaiCli < Formula
     python = "python3.11"
     venv = virtualenv_create(libexec, python)
 
-    # install gnureadline from .whl
-    if Hardware::CPU.arm?
-      venv.pip_install "https://files.pythonhosted.org/packages/83/03/65d82e9290ae8a2a3b2285dc8aebd304437a6ba7ad03823438730525ab45/gnureadline-8.1.2-cp311-cp311-macosx_11_0_arm64.whl"
-    else
-      venv.pip_install "https://files.pythonhosted.org/packages/a7/f2/77195ef94f56b61ad881685e3a87cc39a9972e01ccacd555acaa001a92a0/gnureadline-8.1.2-cp311-cp311-macosx_10_9_x86_64.whl"
+    resources.each do |r|
+      if r.name.eql? "gnureadline"
+        r.stage do
+          if Hardware::CPU.arm?
+            venv.pip_install "gnureadline-8.1.2-cp311-cp311-macosx_11_0_arm64.whl"
+          else
+            venv.pip_install "gnureadline-8.1.2-cp311-cp311-macosx_10_9_x86_64.whl"
+          end
+        end
+      else
+        venv.pip_install r
+      end
     end
-
-    venv.pip_install resources
 
     system libexec/"bin/python", "setup.py", "build"
     system libexec/"bin/python", *Language::Python.setup_install_args(prefix, python)
